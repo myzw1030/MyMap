90ed4a83088e528ee4d543e8a0ab827f0f7524d5
tommy <tmtmtoa@gmail.com>
2023-06-22 22:18:28 +0900

    緯度経度取得（print）

--- MyMap/MapView.swift
+++ MyMap/MapView.swift
@@ -17,6 +17,27 @@ struct MapView: UIViewRepresentable {
         
         // 入力された文字をデバッグエリアに表示
         print("検索キーワード:\(searchKey)")
+        
+        // CLGeocoderインスタンスを生成
+        let geocoder = CLGeocoder()
+        
+        // 入力された文字から位置情報を取得
+        geocoder.geocodeAddressString(
+            searchKey,
+            completionHandler: {(placemarks, error) in
+                // リクエストの結果が存在し、1件目の情報から位置情報を取り出す
+                if let placemarks,
+                   let firstPlacemark = placemarks.first,
+                   let location = firstPlacemark.location {
+                    
+                    // 位置情報から緯度経度をtargetCoordinateに取り出す
+                    let targetCoordinate = location.coordinate
+                    
+                    // 緯度経度をデバッグエリアに表示
+                    print("緯度経度：\(targetCoordinate)")
+                }
+            }
+        )
     }
 }
 
